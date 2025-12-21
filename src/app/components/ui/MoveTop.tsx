dbab213b2640b477450b77f958990131eb0dc97c
luwenbin <linhansong1008@gmail.com>
2025-12-21 21:41:29 +0800

    feat: 修复打包错误

--- src/app/components/ui/MoveTop.tsx
+++ src/app/components/ui/MoveTop.tsx
@@ -2,10 +2,12 @@
 
 import { Icon } from "@iconify/react";
 import { useScroll } from "ahooks";
+import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 
 export default function MoveTop() {
   const scroll = useScroll(() => document);
+  const pathname = usePathname();
 
   const visible = useMemo(() => {
     return (scroll?.top ?? 0) >= 200;
@@ -18,16 +20,18 @@ export default function MoveTop() {
   if (!visible) return null;
 
   return (
-    <div
-      className="fixed bottom-10 right-10 z-50 animate-bounce"
-      onClick={onBackTop}
-    >
-      <Icon
-        icon="akar-icons:paper-airplane"
-        width="28"
-        height="28"
-        className=""
-      />
-    </div>
+    pathname !== "/" && (
+      <div
+        className="fixed bottom-10 right-10 z-50 animate-bounce"
+        onClick={onBackTop}
+      >
+        <Icon
+          icon="akar-icons:paper-airplane"
+          width="28"
+          height="28"
+          className=""
+        />
+      </div>
+    )
   );
 }
