c690f3b9149aa8d52a1defff39441a8b6d32fdfb
luwenbin <linhansong1008@gmail.com>
2026-01-05 15:41:56 +0800

    feat: optimize

--- src/app/components/mdx/MDXComponents.tsx
+++ src/app/components/mdx/MDXComponents.tsx
@@ -10,10 +10,10 @@ import Logo from "@/app/components/ui/Logo";
 import Image from "@/app/components/ui/Image";
 import SvgHover from "@/app/components/others/SvgHover";
 import RetinaLine from "@/app/components/others/RetinaLine";
-import RuleDropShadow from "../others/RuleDropShadow";
-import GhostAnimation from "../others/GhostAnimation";
-import CircleShadowLoading from "../others/CircleShadowLoading";
-import BallBlurLoading from "../others/BallBlurLoading";
+import RuleDropShadow from "@/app/components/others/RuleDropShadow";
+import GhostAnimation from "@/app/components/others/GhostAnimation";
+import CircleShadowLoading from "@/app/components/others/CircleShadowLoading";
+import BallBlurLoading from "@/app/components/others/BallBlurLoading";
 
 type MDXComponents = ComponentProps<typeof MDXRemote>["components"];
 
